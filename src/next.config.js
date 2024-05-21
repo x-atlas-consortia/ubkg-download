23f9e9446849b4700e6f2aa52231233823b5a473
Birdmachine <jeswaldrip@gmail.com>
2024-05-21 11:45:00 -0400

    Fix the Fix for the configs with the proper values saved

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    reactStrictMode: false,
+    reactStrictMode: true,
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
