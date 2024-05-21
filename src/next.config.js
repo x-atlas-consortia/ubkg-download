8c7fb09999b352dbaa449555d54f862b5ad9b7f5
Birdmachine <jeswaldrip@gmail.com>
2024-05-21 11:43:22 -0400

    Revert changes to error checks

--- src/next.config.js
+++ src/next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    reactStrictMode: true,
+    reactStrictMode: false,
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
-        ignoreDuringBuilds: false,
+        ignoreDuringBuilds: true,
     },
     typescript: {
-        ignoreBuildErrors: false,
+        ignoreBuildErrors: true,
     },
 };
