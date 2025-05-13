a808068ff1b33ecf13c48f52f8ae99024d83e31d
Birdmachine <jeswaldrip@gmail.com>
2025-05-13 11:47:09 -0400

    Update Sourcing of File information to use the assets server, rebuild table based on context and type information, fix loading UMLS from current session and re-enable reset feature for key input

--- src/api/files.js
+++ src/api/files.js
@@ -15,40 +15,20 @@ export async function GET({
 }
 
 
-export async function FileList(umlsKey) {
-    let url = `${process.env.NEXT_PUBLIC_API_URL_BASE}${process.env.NEXT_PUBLIC_UBKG_FILES_ENDPOINT}?umls-key=${umlsKey}`
-    let headers = {
-        "Content-Type": "application/json"
-    }
-    console.debug('%c◉ fileList url ', 'color:#00ff7b', url);
-
-
-    return await fetch(
-        url, {
+export async function FileListProviderUpgrade(umlsKey) {
+    let url = `${process.env.NEXT_PUBLIC_ASSETS_URL_BASE}file_descriptions.json?umls-key=${umlsKey}`;
+    try {
+        const response = await fetch(url, {
             method: "GET",
-            headers: headers,
-        })
-        // @TODO
-        // We may need to switch out Data with Result, 
-        // Had to use data for the local file list
-        // Result should be correct for the API Response
-
-        // RESULT
-        .then(result => {
-            console.debug('%c◉ fileList result ', 'color:#00ff7b', result);
-            return result;
-        })
-        .then(function (response) {
-            return response.text();
-        })
-        .then(function (data) {
-            // console.debug('%c◉ response ', 'color:#00ff7b', response);
-            console.debug('%c◉ fileList result ', 'color:#00ff7b', data);
-            // log.info(result)
-            return data;
-        }).catch(error => {
-            console.debug('%c◉ fileList error ', 'color:#00ff7b', error);
-            // log.error('error', error)
-            return error;
+            headers: {"Content-Type": "application/json"}
         });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const jsonData = await response.json();
+        return jsonData; // Return the parsed JSON data
+    } catch (error) {
+        console.error('Error fetching JSON file:', error);
+        throw error; // Re-throw the error to handle it in the calling code
+    }
 }
